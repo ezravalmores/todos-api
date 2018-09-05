4d26561abb126e100b29af98325684b11819ff53
ezravalmores <ezravalmores@gmail.com>
2018-09-05 20:51:18 +0800

    Added route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :todos
+  resources :todos do
+    collection do
+      get 'get_completed'
+    end
+  end
 end
